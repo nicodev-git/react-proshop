aa2e55429084349b4b3483d0193ccd6d12e30b81
nicodev-git <schaefernicker@gmail.com>
2021-02-09 14:27:02 +0200

    Adding Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from '../src/screens/HomeScreen'
 const App = () => {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          {' '}
-          <h1>Welcome To ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
     </>
   )
 }
-
 export default App
