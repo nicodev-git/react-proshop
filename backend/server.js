1cfd399d71380a46e80e21e7be8e52bacc185c09
nicodev-git <schaefernicker@gmail.com>
2021-02-18 19:15:22 +0200

    Create dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const express=require('express')
+// require and configure dotenv
+const dotenv=require('dotenv')
+
 const products=require('./Data/products')
+
+
+dotenv.config()
+
 const app=express()
 app.get('/',(req,res)=>{
     res.send('API is running ...')
@@ -11,4 +18,6 @@ app.get('/api/products/:id',(req,res)=>{
     const product=products.find((p)=>p._id === req.params.id)
     res.json(product)
 })
-app.listen(5000,console.log('Server Running on Port 5000'))
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`))
