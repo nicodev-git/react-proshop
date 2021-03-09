6ed57f3d9afebc1650d6804e989f009e06d468ac
nicodev-git <schaefernicker@gmail.com>
2021-03-09 17:40:44 +0200

    code Formatting

--- backend/Models/orderModel.js
+++ backend/Models/orderModel.js
@@ -1,82 +1,82 @@
 import mongoose from 'mongoose'
- const {Schema}=mongoose
+const { Schema } = mongoose
 
-const orderSchema=new Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'User'
-
+const orderSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-      orderItems:[{
-        name:{type:String,required:true},
-         qty:{type:Number,required:true},
-          image:{type:String,required:true},
-           price:{type:Number,required:true},
-            product:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Product'},
-             
-        
-    }],
-      shippingAddress:{
-       address:{type:String,required:true},
-       city:{type:String,required:true},
-       postalCode:{type:String,required:true},
-       country:{type:String,required:true},
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
-      paymentMethod:{
-        type:String,
-        required:true,
-        
+    paymentMethod: {
+      type: String,
+      required: true,
     },
-      paymentResult:{
-        id:{type:String},
-         status:{type:String},
-          update_time:{type:String},
-          email_address:{type:String}
-        
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
-      taxPrice:{
-        type:Number,
-        required:true,
-        default:0.0
-        
-    }, taxPrice:{
-        type:Number,
-        required:true,
-        default:0.0
-        
-    }, shippingPrice:{
-        type:Number,
-        required:true,
-        default:0.0
-        
-    }, totalPrice:{
-        type:Number,
-        required:true,
-        default:0.0
-        
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-    isPaid:{
-        type:Boolean,
-        required:true,
-        default:false
-        
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-     paidAt:{
-        type:Date,
-        
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-    isDelivered:{ 
-      type:Boolean,
-        required:true,
-        default:false
-      }, deliveredAt:{
-        type:Date,
-        
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-  
-},{
-    timestamps:true
-}) 
-const Order=mongoose.model('Order',orderSchema)
-export default Order;
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Order = mongoose.model('Order', orderSchema)
+export default Order
--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,27 +1,30 @@
 import mongoose from 'mongoose'
- const {Schema}=mongoose
+const { Schema } = mongoose
 
-const userSchema=new Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-      email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-      password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-      isAdmin:{
-        type: Boolean,
+    isAdmin: {
+      type: Boolean,
       required: true,
       default: false,
     },
-},{
-    timestamps:true
-}) 
-const User=mongoose.model('User',userSchema)
-export default User;
+  },
+  {
+    timestamps: true,
+  }
+)
+const User = mongoose.model('User', userSchema)
+export default User
