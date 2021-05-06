92e6e9cb111931c80a69909af2e500a505efc3d0
nicodev-git <schaefernicker@gmail.com>
2021-05-06 10:22:08 +0200

    User Auth

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 const { Schema } = mongoose
 
 const userSchema = new Schema(
@@ -26,5 +27,9 @@ const userSchema = new Schema(
     timestamps: true,
   }
 )
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
 const User = mongoose.model('User', userSchema)
 export default User
