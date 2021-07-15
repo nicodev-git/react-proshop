56af66217e4078cd27e72157cafa20f7ebc9efaa
nicodev-git <schaefernicker@gmail.com>
2021-07-15 13:49:42 +0200

    Shipping Screen & Save Address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from '../src/screens/CartScreen'
 import LoginScreen from '../src/screens/LoginScreen'
 import RegisterScreen from '../src/screens/RegisterScreen'
 import ProfileScreen from '../src/screens/ProfileScreen'
+import ShippingScreen from '../src/screens/ShippingScreen'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+        <Route path='/shipping' component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
