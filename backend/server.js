a735539c0bfc8fc8c2777c8295afcfb5fd44c835
nicodev-git <schaefernicker@gmail.com>
2021-07-16 23:40:17 +0200

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -14,11 +15,12 @@ connectDB()
 const app = express()
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('API is running ...')
+	res.send('API is running ...')
 })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 
@@ -26,8 +28,8 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
-  PORT,
-  console.log(
-    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
-  )
+	PORT,
+	console.log(
+		`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+	)
 )
