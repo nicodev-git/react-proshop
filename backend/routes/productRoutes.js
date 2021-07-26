0c27d2790f6153517cc1c3a66efe12c3631493e9
nicodev-git <schaefernicker@gmail.com>
2021-07-26 14:16:42 +0200

    Create & update Products Endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,12 +4,18 @@ import {
 	getProducts,
 	getProductById,
 	deleteProduct,
+	updateProduct,
+	createProduct,
 } from '../controllers/productController.js'
 
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
-router.get('/', getProducts)
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
 
-router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct)
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, isAdmin, deleteProduct)
+	.put(protect, isAdmin, updateProduct)
 
 export default router
