1c6b42af954d8dac1f2552a95e48ce9a3ebe7dad
nicodev-git <schaefernicker@gmail.com>
2021-07-26 10:06:07 +0200

    User Edit Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from '../src/screens/PaymentScreen'
 import PlaceOrderScreen from '../src/screens/PlaceOrderScreen'
 import OrderScreen from '../src/screens/orderScreen'
 import UserListScreen from '../src/screens/UserListScreen'
+import UserEditScreen from '../src/screens/UserEditScreen'
 
 const App = () => {
 	return (
@@ -31,6 +32,7 @@ const App = () => {
 					<Route path="/product/:id" component={ProductScreen} />
 					<Route path="/cart/:id?" component={CartScreen} />
 					<Route path="/admin/userlist" component={UserListScreen} />
+					<Route path="/admin/user/:id/edit" component={UserEditScreen} />
 					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
