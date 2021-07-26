a32eceb4eec8f1388de12567dca9d274f49f9c89
nicodev-git <schaefernicker@gmail.com>
2021-07-26 19:07:45 +0200

    Admin Create Product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 
-import { ListProducts, deleteProduct } from '../actions/productActions'
+import {
+	ListProducts,
+	deleteProduct,
+	createProduct,
+} from '../actions/productActions'
+
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
 	const dispatch = useDispatch()
@@ -16,6 +22,14 @@ const ProductListScreen = ({ history, match }) => {
 	const productList = useSelector((state) => state.productList)
 	const { loading, error, products } = productList
 
+	const productCreate = useSelector((state) => state.productCreate)
+	const {
+		loading: loadingCreate,
+		error: errorCreate,
+		success: successCreate,
+		product: createdProduct,
+	} = productCreate
+
 	const productDelete = useSelector((state) => state.productDelete)
 	const {
 		loading: loadingDelete,
@@ -24,12 +38,24 @@ const ProductListScreen = ({ history, match }) => {
 	} = productDelete
 
 	useEffect(() => {
-		{
-			userInfo && userInfo.isAdmin
-				? dispatch(ListProducts())
-				: history.push('/login')
+		dispatch({ type: PRODUCT_CREATE_RESET })
+		if (!userInfo.isAdmin) {
+			history.push('/login')
 		}
-	}, [dispatch, history, userInfo, successDelete])
+
+		{
+			successCreate
+				? history.push(`/admin/product/${createdProduct._id}/edit`)
+				: dispatch(ListProducts())
+		}
+	}, [
+		dispatch,
+		history,
+		userInfo,
+		successDelete,
+		successCreate,
+		createdProduct,
+	])
 
 	const deleteHandler = (id) => {
 		if (window.confirm('Are you sure?')) {
@@ -38,8 +64,9 @@ const ProductListScreen = ({ history, match }) => {
 		}
 	}
 
-	const createProductHandler = (product) => {
+	const createProductHandler = () => {
 		//CREATE PRODUCT
+		dispatch(createProduct())
 	}
 	return (
 		<>
@@ -55,6 +82,8 @@ const ProductListScreen = ({ history, match }) => {
 			</Row>
 			{loadingDelete && <Loader />}
 			{errorDelete && <Message variant="danger">{errorDelete}</Message>}
+			{loadingCreate && <Loader />}
+			{errorCreate && <Message variant="danger">{errorCreate}</Message>}
 			{loading ? (
 				<Loader />
 			) : error ? (
