821976dcddf56c4e345ec37edc1605c622a2ddbb
nicodev-git <schaefernicker@gmail.com>
2021-07-26 09:27:38 +0200

    edit the edit path & showing decoded

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -16,7 +16,6 @@ const protect = asyncHandler(async (req, res, next) => {
 			const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
 			req.user = await User.findById(decoded.id).select('-password')
-			console.log(decoded)
 
 			next()
 		} catch (error) {
