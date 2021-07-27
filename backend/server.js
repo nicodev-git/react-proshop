df0bad75286c04b4eb5dabcd8fa2e4d499d74c5d
nicodev-git <schaefernicker@gmail.com>
2021-07-27 10:13:17 +0200

    Image upload config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-// require and configure dotenv
+import path from 'path'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -8,6 +8,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes'
 
 dotenv.config()
 connectDB()
@@ -21,10 +22,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_Client_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
