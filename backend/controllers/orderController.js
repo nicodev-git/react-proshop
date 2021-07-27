19334460674f9602beb7617a05043cff0ae099f2
nicodev-git <schaefernicker@gmail.com>
2021-07-27 13:50:40 +0200

    deliveroder endpoint & mark as delivered fronted

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,23 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 	}
 })
 
+// @desc  Update Order to delivered
+// @route PUT /api/orders/:id/deliver
+// @access Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id)
+	if (order) {
+		order.isDelivered = true
+		order.deliveredAt = Date.now()
+		const updatedOrder = order.save()
+		res.json(updatedOrder)
+	} else {
+		res.status(404)
+		throw new Error('Order not found')
+	}
+})
+
 // @desc Get Logged In User Orders
 // @route GET  /api/orders/myorders
 // @access Private
@@ -102,4 +119,5 @@ export {
 	updateOrderToPaid,
 	getMyOrders,
 	getOrders,
+	updateOrderToDelivered,
 }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -8,6 +8,7 @@ import {
 	updateOrderToPaid,
 	getMyOrders,
 	getOrders,
+	updateOrderToDelivered,
 } from '../controllers/orderController.js'
 
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
@@ -19,5 +20,7 @@ router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+
+router.route('/:id/deliver').put(protect, isAdmin, updateOrderToDelivered)
 
 export default router
