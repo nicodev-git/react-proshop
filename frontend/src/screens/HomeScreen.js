bd6558b19cfc39a6ad98ef828a272e3d643cd3a2
nicodev-git <schaefernicker@gmail.com>
2021-07-28 12:31:07 +0200

    Product Search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,32 +6,36 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { ListProducts } from '../actions/productActions'
 
-const HomeScreen = () => {
-  const dispatch = useDispatch()
-  const productList = useSelector((state) => state.productList)
-  const { loading, products, error } = productList
-  useEffect(() => {
-    dispatch(ListProducts())
-  }, [dispatch])
+const HomeScreen = ({ match }) => {
+	const keyword = match.params.keyword
 
-  return (
-    <>
-      <h1>Latest products</h1>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>{error}</Message>
-      ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
-  )
+	const dispatch = useDispatch()
+
+	const productList = useSelector((state) => state.productList)
+	const { loading, products, error } = productList
+
+	useEffect(() => {
+		dispatch(ListProducts(keyword))
+	}, [dispatch, keyword])
+
+	return (
+		<>
+			<h1>Latest products</h1>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
+		</>
+	)
 }
 
 export default HomeScreen
